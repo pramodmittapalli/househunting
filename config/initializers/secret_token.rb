8c4149719f896f968b1ae62619749db44ed5db0b
pramod mittapalli <pramod.mittapalli@yahoo.com>
2010-08-13 09:37:09 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Househunting::Application.config.secret_token = 'a60e089d3976640f2a00e8621562ea5ae5e6a13a6e901b0dfbf8bf778090c37d8ab06e901cb540e8c6712be69c13163d36ea94a2e484add7f39e26cb79d1369b'
